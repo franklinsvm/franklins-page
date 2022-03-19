d3780b31cd1a4bb4c8223c0c06a77930ee222c06
Franklin Martins <franklinsvmartins@hotmail.com>
2022-03-19 17:31:06 -0300

    Navbar update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,28 +12,28 @@ const NavBar = () => {
     
   return (
     <>
-    <div className="navbar">
-        <ion-icon className="menu-bars" color="light" name="menu-outline" onClick={showSidebar}></ion-icon>
-    </div>
-    <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-            <div className="navbar-toggle">
-                <ion-icon className="close" color="light" name="close-outline" onClick={showSidebar}></ion-icon>
-            </div>
-        <ul className="nav-menu-items" onClick={showSidebar}>
-            {SideBarData.map((item, index) => {
-                return (
-                    <>
-                    <li key={index} className={item.cName}>
-                        <Link smooth to={item.path}>
-                            {item.icon}
-                            <span className="item-title">{item.title}</span>
-                        </Link>
-                    </li>
-                    </>
-                )
-            })}
-        </ul>
-    </nav>
+        <div className="navbar">
+            <ion-icon className="menu-bars" color="light" name="menu-outline" onClick={showSidebar}></ion-icon>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                <div className="navbar-toggle">
+                    <ion-icon className="close" color="light" name="close-outline" onClick={showSidebar}></ion-icon>
+                </div>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+                {SideBarData.map((item, index) => {
+                    return (
+                        <>
+                        <li key={index} className={item.cName}>
+                            <Link smooth to={item.path}>
+                                {item.icon}
+                                <span className="item-title">{item.title}</span>
+                            </Link>
+                        </li>
+                        </>
+                    )
+                })}
+            </ul>
+        </nav>
     </>
   )
 }
