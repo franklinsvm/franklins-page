625932f10d4701cca5c713038a8a4cd1b3248ef9
Franklin Martins <franklinsvmartins@hotmail.com>
2022-04-16 11:14:12 -0300

    Portfolio update

--- src/Components/PortfolioPage.jsx
+++ src/Components/PortfolioPage.jsx
@@ -13,12 +13,12 @@ const PortfolioPage = () => {
                 <PortfolioItem src="Imgs/google-clone.png" text="Google Clone" url="https://f-google-clone.herokuapp.com/search" />
             </ul>
             <ul className="portfolio-items portfolio-middle" >
-                {/* <PortfolioItem src="logo512.png" text="Spreadsheet" /> */}
+                <PortfolioItem src="Imgs/spreadsheet.png" text="Spreadsheet" url="https://f-spreadsheet.herokuapp.com/" />
                 <div className="portfolio-center">
                     <img src="logo512.png" className="portfolio-img" alt="/" />
                     <span>My Projects!</span>
                 </div>
-                {/* <PortfolioItem src="logo512.png" text="Travel Planner" /> */}
+                <PortfolioItem src="logo512.png" text="Under Construction..." />
             </ul>
             <ul className="portfolio-items portfolio-bottom" >
                 <PortfolioItem src="Imgs/calculator.png" text="Calculator" url="https://f-calculator.herokuapp.com/" />
