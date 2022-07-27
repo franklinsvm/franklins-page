543f6f5d82c090b86c4ad91224a8d1c2c32365d8
Franklin Martins <franklinsvmartins@hotmail.com>
2022-07-27 15:49:01 -0300

    CV icons add

--- src/Components/Home/CV.jsx
+++ src/Components/Home/CV.jsx
@@ -15,9 +15,13 @@ const CV = () => {
         </div>
         <div className="curriculum-options">
             <div className="curriculum-option">
+            <div>
+            <ion-icon name="document-text-outline" color="light" size="large" ></ion-icon>
+            </div>
             <h6>You can check my Curriculum Vitae <a href="FM CV Dev.pdf" target="_blank">here!</a></h6>    
             </div>
             <div className="curriculum-option">
+            <ion-icon name="add-circle-outline" color="light" size="large" ></ion-icon>
             <h6>You can help me IMPROVE my Curriculum Vitae<button className="btn-cv" onClick={onClickCV}>here!</button></h6>    
             </div>
         </div>
