8c4cf8719254040a8cce864605dcaa8b82df4f55
Franklin Martins <franklinsvmartins@hotmail.com>
2022-10-27 19:59:34 -0300

    Hero Section Update

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import './HeroSection.css';
+import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
+  const onClickLets = () => {
+    const position = document.getElementById("expertise-id").offsetTop;
+    window.scrollTo({ top: position });
+  };
 
-    const onClickLets = () => {
-        const position = document.getElementById("expertise-id").offsetTop;
-        window.scrollTo({top: position})
-    }
-
-    return (
-        <div className="hero-container" id="hero-id">
-            <video src="Imgs/space-video.mp4" autoPlay loop muted />
-            <div className="hero-back">
-                <h1>Mr. Franklin Martins</h1>
-                <p>Your Full Stack Developer!</p>
-                <div className="hero-btn">
-                    <button className="btn-hero" onClick={onClickLets}>Let's GO!</button>
-                </div>
-            </div>
+  return (
+    <div className="hero-container" id="hero-id">
+      <video src="Imgs/space-video.mp4" autoPlay loop muted />
+      <div className="hero-back">
+        <div className="stars">
+          <div className="star-icon star-1">
+            <img className="star" src="Imgs/star-f.png" />
+          </div>
+          <div className="star-icon star-2">
+            <img className="star" src="Imgs/star-f.png" />
+          </div>
+          <div className="star-icon star-3">
+            <img className="star" src="Imgs/star-f.png" />
+          </div>
+          <div className="star-icon star-4">
+            <img className="star" src="Imgs/star-f.png" />
+          </div>
+          <div className="star-icon star-5">
+            <img className="star" src="Imgs/star-e.png" />
+          </div>
         </div>
-    )
-}
+        <h1>Mr. Franklin Martins</h1>
+        <p>Your Frontend (soon to be Full Stack) Developer!</p>
+      </div>
+      <div className="hero-btn">
+        <button className="btn-hero" onClick={onClickLets}>
+          Let's GO!
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default HeroSection;
